c879b3f433e7fad84a5b85a119ded744b874af92
Meet Bhalodi <42618929+mit-bhalodi@users.noreply.github.com>
2021-04-03 20:45:49 +0530

    about us / contact us #6
    both pages are completed and responsive
    
    Co-Authored-By: prakhar-111 <78064327+prakhar-111@users.noreply.github.com>

---
+++ src/AboutUs.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./AboutUs.css";
+//import ProductBox from "./ProductBox";
+import Navbar from "./Navbar";
+import image2 from "./image_abt_us@2x.png";
+//import image1 from "./pattern.png";
+
+export default function AboutUs() {
+  return (
+    <div className="main__container">
+      <Navbar />
+      <div className="inner__container">
+        <div className="inner__para">
+          <h1>About Us</h1>
+          <h5>
+            There is nothing much to know about us. but since you have really
+            took an effort to click on the button, let me tell you something.
+            <strong> we are the one! </strong> <br />
+            we are group of individuals who has worked very hard to make
+            difference without caring the people’s response. There nothing in
+            this project that you really hate so We really expect you towards
+            your positive response.{" "}
+          </h5>
+        </div>
+
+        <div className="side__images">
+          <img src={image2} alt="gathering" className="image1"></img>
+        </div>
+      </div>
+    </div>
+  );
+}
