8108bfcae283c3e17620acf41d388d8c8bc8ccc5
Aayush Desai <aayushdesai01@gmail.com>
2021-05-04 00:38:38 +0530

    Update sellerHistory.js

--- backend/routes/sellerHistory.js
+++ backend/routes/sellerHistory.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const sellerHistory = require("../service/sellerHistory");
+const verify = require("../service/verify");
 //const verify = require("../service/verifyToken1");
 
-router.get("/getHistory", sellerHistory.getHistory);
+router.get("/getHistory",verify.verify, sellerHistory.getHistory);
 
 module.exports = router;
