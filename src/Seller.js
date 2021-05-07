29ea154913824224a2f5d1e69e853207144597e8
Meet Bhalodi <42618929+mit-bhalodi@users.noreply.github.com>
2021-05-07 10:47:08 +0530

    seller page not tested

--- src/Seller.js
+++ src/Seller.js
@@ -13,7 +13,6 @@ import buyBook from "../src/service/buy/BuyBook";
 import Modal from "@material-ui/core/Modal";
 import { useForm } from "react-hook-form";
 import { makeStyles } from "@material-ui/core/styles";
-
 import { AuthContext } from './App';
 
 
