02bf5d8adff19aa9246e054e99ca8caf20322d60
Aayush Desai <aayushdesai01@gmail.com>
2021-05-09 22:39:31 +0530

    heroku update

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const bodyparser = require("body-parser");
 const session = require('express-session');
+const path = require('path');
+
 // Routes
 const auth = require("./routes/auth");
 const sellerHistory = require("./routes/sellerHistory");
@@ -19,7 +21,24 @@ app.use(session({
 	saveUninitialized: true,
 	maxAge: 30 * 86400 * 1000
 }));
-app.use(cors());
+
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:5000', 'https://group-21-book-exchange.herokuapp.com/']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+// --> Add this
+app.use(cors(corsOptions))
+
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
@@ -37,6 +56,16 @@ app.all("*", (req, res) => {
   return res.json({ success: false, err_code: 404, message: "Invalid URL!" });
 });
 
-app.listen(process.env.port || 5000, () => {
+// --> Add this
+if (process.env.NODE_ENV === 'production') {
+	// Serve any static files
+	app.use(express.static(path.join(__dirname, '../build')));
+  // Handle React routing, return all requests to React app
+	app.get('*', function(req, res) {
+	  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+	});
+  }
+
+app.listen(process.env.PORT || 5000, () => {
   console.log("Server has Started on port 5000");
 });
