3f036a8ba51a52ae61399aa8808b5d779ab8dda1
Aayush Desai <aayushdesai01@gmail.com>
2021-05-31 22:56:34 +0530

    hosting trial

--- backend/index.js
+++ backend/index.js
@@ -23,21 +23,9 @@ app.use(session({
 }));
 
 // ** MIDDLEWARE ** //
-const whitelist = ['http://localhost:5000/', 'https://group-21-book-exchange.herokuapp.com/']
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
-    } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+
 // --> Add this
-app.use(cors(corsOptions))
+app.use(express.static(__dirname + `../build/`));
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
